01419e5ef7ebcba3fd78aeaa76ab39215b4a2e03
Jawad <jawad_1422@hotmail.com>
2023-02-17 21:52:23 +0300

    up

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,18 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../data/models/User.dart';
+
 class DatabaseService {
   final String uid;
   DatabaseService({required this.uid});
-  // late User user;
+  late User user;
   final CollectionReference customerCollection =
       FirebaseFirestore.instance.collection('Customer');
   Future updateUserData(
       String email, String name, String city, String phoneNumber) async {
-    // user = user(
-        // customerID: uid,
-        // email: email,
-        // phone: phoneNumber,
-        // );
-    // return await customerCollection.doc(uid).set(customer.toJson());
+    user = User(
+        id: uid,
+        phoneNumber: phoneNumber,
+        
+        );
+    return await customerCollection.doc(uid).set(user.toJson());
   }
 }
